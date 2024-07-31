d7565489e10bce77d45cb55e1f2b7476b070d635
Neha Yerragudi <neha.s.yerragudi@kipi.bi>
2024-07-31 10:54:55 +0530

    insert 3 rows

--- migrations/V1.1.3__alter_obj.sql
+++ migrations/V1.1.3__alter_obj.sql
@@ -6,3 +6,6 @@ VALUES ('sai','sri',5);
 INSERT INTO HELLO_WORLD ( first_name,last_name,age)
 VALUES ('john','mac',5);
 
+INSERT INTO HELLO_WORLD ( first_name,last_name,age)
+VALUES ('mark','antony',20);
+
